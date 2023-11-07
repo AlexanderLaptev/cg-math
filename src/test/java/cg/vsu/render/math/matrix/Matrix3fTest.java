a59b0568eb06b01901d7f95e0f70bc532c1de551
Alexander Laptev <alexander_laptev4@inbox.ru>
2023-11-07 22:09:55 +0300

    feat: implement Matrix2f and Matrix3f

---
+++ src/test/java/cg/vsu/render/math/matrix/Matrix3fTest.java
@@ -0,0 +1,75 @@
+package cg.vsu.render.math.matrix;
+
+import cg.vsu.render.math.MathUtils;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class Matrix3fTest {
+    private static final float[] VALUES_1 = {
+            3.0f, 5.0f, 7.0f,
+            -2.0f, 1.0f, 4.0f,
+            -4.0f, -2.0f, 5.0f
+    };
+
+    private static final float[] VALUES_2 = {
+            2.0f, 4.0f, 1.0f,
+            -3.0f, 5.0f, 2.0f,
+            -1.0f, -2.0f, -3.0f
+    };
+
+    private static final float DET_1 = 65.0f;
+    private static final float DET_2 = -55.0f;
+
+    private Matrix3f m1 = new Matrix3f();
+    private Matrix3f m2 = new Matrix3f();
+
+    @Test
+    void unitMatrixDeterminantIsOne() {
+        m1.setIdentity();
+
+        assertEquals(1.0f, m1.det());
+    }
+
+    @Test
+    void detReturnsExpectedValue() {
+        m1.set(VALUES_1);
+        m2.set(VALUES_2);
+
+        assertEquals(DET_1, m1.det());
+        assertEquals(DET_2, m2.det());
+    }
+
+    @Test
+    void transposeDoesNotAffectDeterminant() {
+        m1.set(VALUES_1);
+        m2.set(m1.cpy().tra());
+
+        assertEquals(m1.det(), m2.det());
+    }
+
+    @Test
+    void transposedMultiplication() {
+        m1.set(VALUES_1);
+        m2.set(VALUES_2);
+
+        Matrix3f r1 = m1.cpy().mul(m2).tra();
+        Matrix3f r2 = m2.cpy().tra().mul(m1.cpy().tra());
+
+        assertEquals(r1, r2);
+    }
+
+    @Test
+    void inverseMatrixProperties() {
+        m1.set(VALUES_1);
+        m2 = m1.cpy().inv();
+
+        Matrix3f r1 = m1.cpy().mul(m2);
+        Matrix3f r2 = m2.cpy().mul(m1);
+
+        assertTrue(r1.epsEquals(r2, MathUtils.EPSILON));
+        assertTrue(r1.epsEquals(Matrix3f.identity(), MathUtils.EPSILON));
+        assertTrue(r2.epsEquals(Matrix3f.identity(), MathUtils.EPSILON));
+    }
+}
